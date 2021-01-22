06ee07548501a60a145867250797b8c6c3dae0b8
PMacStew <23pmcclure@athenian.org>
2021-01-22 05:54:09 +0000

    done

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,10 +1,11 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class MyMain {
     // This method returns true/false if there 
     // was a boat the specified coordinates. This
     // method also prints out an appropriate message
     public static boolean hit(boolean[][] board, int row, int col) { 
-        // YOUR CODE HERE
-        return false;
+        return (board[row][col] == true) ? true : false;
     }
 
 
@@ -13,7 +14,12 @@ public class MyMain {
     // The remaining pieces are placed in the direction given
     // by the direction input
     public static boolean[][] placeBoat(boolean[][] board, String direction, int boatLength, int row, int col) { 
-        // YOUR CODE HERE
+        for (int i = 0; i < boatLength; i++) {
+            if (direction.equals("down"))
+                board[row + i][col] = true;
+            else 
+                board[row][col + i] = true;
+        }
         return board;
     }
 
@@ -22,11 +28,33 @@ public class MyMain {
     // increasing length
     // You may assume that all Strings are lowercase 
     public static boolean inOrder(String[][] words) { 
-        // YOUR CODE HERE
-        return false;
+        String temp = "";
+        for (String[] row : words) {
+            temp = row[0];
+            for (String word : row) {
+                if (!alphabeticalOrder(temp, word))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean alphabeticalOrder(String word1, String word2) {
+        if (word1.length() > word2.length())
+            return false;
+        for (int i = 0; i < word2.length(); i++) {
+            if (word1.charAt(i) > word2.charAt(i))
+                return false;
+            else if (word1.charAt(i) == word2.charAt(i))
+                continue;
+            else 
+                return true;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        // You can test your code here
+        System.out.println(alphabeticalOrder("anchor", "boating"));
+
     }
 }
